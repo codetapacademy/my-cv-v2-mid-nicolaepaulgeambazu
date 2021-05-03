7d6c1d38569f37803672550eb822d349fc3b83be
paulgsm <58441846+paulgsm@users.noreply.github.com>
2021-05-03 21:52:09 +0100

    All Done now

---
+++ src/component/dist/theme.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.darkTheme = exports.lightTheme = void 0;
+var lightTheme = {
+  body: '#FFF',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  background: '#363537'
+};
+exports.lightTheme = lightTheme;
+var darkTheme = {
+  body: '#363537',
+  text: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  background: '#999'
+};
+exports.darkTheme = darkTheme;
